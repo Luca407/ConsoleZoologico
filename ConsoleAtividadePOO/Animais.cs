861bb6bedfef7a2cb1a06dd35acb9351f406c110
Luca407 <0001081825@senaimgaluno.com.br>
2025-05-31 15:26:27 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAtividadePOO/Animais.cs
@@ -0,0 +1,111 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAtividadePOO
+{
+    public abstract class Animal
+    {
+        public string Nome { get; set; }
+
+        private int idade;
+        public int Idade
+        {
+            get => idade;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("A idade não pode ser negativa.");
+                idade = value;
+            }
+        }
+
+        public abstract void EmitirSom();
+
+        public virtual void Apresentar()
+        {
+            Console.WriteLine($"Nome: {Nome}, Idade: {Idade}");
+        }
+    }
+
+    // Cachorro
+    public class Cachorro : Animal
+    {
+        public override void EmitirSom()
+        {
+            Console.WriteLine("Au Au!");
+        }
+    }
+
+    // Gato
+    public class Gato : Animal
+    {
+        public override void EmitirSom()
+        {
+            Console.WriteLine("Miau!");
+        }
+    }
+
+    // Pássaro
+    public class Passaro : Animal
+    {
+        public override void EmitirSom()
+        {
+            Console.WriteLine("Piu Piu!");
+        }
+
+        public void Voar()
+        {
+            Console.WriteLine($"{Nome} está voando!");
+        }
+    }
+
+    // Zoo (Composição)
+    public class Zoo
+    {
+        private List<Animal> animais = new List<Animal>();
+
+        public void AdicionarAnimal(Animal animal)
+        {
+            animais.Add(animal);
+        }
+
+        public void MostrarAnimais()
+        {
+            foreach (var animal in animais)
+            {
+                animal.Apresentar();
+                animal.EmitirSom();
+            }
+        }
+
+        public Animal BuscarPorNome(string nome)
+        {
+            return animais.Find(a => a.Nome.Equals(nome, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+
+    // Veterinário
+    public class Veterinario
+    {
+        private string especialidade;
+        public string Especialidade
+        {
+            get => especialidade;
+            set
+            {
+                if (value != "Caninos" && value != "Felinos" && value != "Aves")
+                    throw new ArgumentException("Especialidade inválida.");
+                especialidade = value;
+            }
+        }
+
+        public void Examinar(Animal animal)
+        {
+            Console.WriteLine("Veterinário está examinando o animal...");
+            animal.EmitirSom();
+        }
+    }
+}
---
+++ ConsoleAtividadePOO/Program.cs
@@ -0,0 +1,89 @@
+﻿using ConsoleAtividadePOO;
+using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Zoo zoo = new Zoo();
+        bool continuar = true;
+
+        while (continuar)
+        {
+            Console.WriteLine("\n--- MENU ZOOLÓGICO ---");
+            Console.WriteLine("1. Adicionar Cachorro");
+            Console.WriteLine("2. Adicionar Gato");
+            Console.WriteLine("3. Adicionar Pássaro");
+            Console.WriteLine("4. Mostrar Animais");
+            Console.WriteLine("5. Examinar com Veterinário");
+            Console.WriteLine("0. Sair");
+            Console.Write("Escolha uma opção: ");
+            string opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    zoo.AdicionarAnimal(CriarAnimal(new Cachorro()));
+                    break;
+                case "2":
+                    zoo.AdicionarAnimal(CriarAnimal(new Gato()));
+                    break;
+                case "3":
+                    zoo.AdicionarAnimal(CriarAnimal(new Passaro()));
+                    break;
+                case "4":
+                    zoo.MostrarAnimais();
+                    break;
+                case "5":
+                    Veterinario vet = new Veterinario();
+                    Console.Write("Digite a especialidade do veterinário (Caninos, Felinos, Aves): ");
+                    try
+                    {
+                        vet.Especialidade = Console.ReadLine();
+                        Console.Write("Digite o nome do animal a ser examinado: ");
+                        string nome = Console.ReadLine();
+                        Animal animal = zoo.BuscarPorNome(nome);
+                        if (animal != null)
+                            vet.Examinar(animal);
+                        else
+                            Console.WriteLine("Animal não encontrado.");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Erro: {ex.Message}");
+                    }
+                    break;
+                case "0":
+                    continuar = false;
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida.");
+                    break;
+            }
+        }
+    }
+
+    static Animal CriarAnimal(Animal animal)
+    {
+        Console.Write("Digite o nome: ");
+        animal.Nome = Console.ReadLine();
+
+        bool idadeValida = false;
+        while (!idadeValida)
+        {
+            Console.Write("Digite a idade: ");
+            try
+            {
+                animal.Idade = int.Parse(Console.ReadLine());
+                idadeValida = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
+        }
+
+        return animal;
+    }
+}
